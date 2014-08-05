cf9a221f7258c5072e972da7cf30fbe708ed2aa4
LexShade <lex.schade@gmail.com>
2014-08-05 12:02:34 +0200

    Initial commit.

---
+++ MyTestProject8/src/de/lex/dynamics/main/Launch.java
@@ -0,0 +1,21 @@
+package de.lex.dynamics.main;
+
+/**
+ * The main class for launching the application.
+ * 
+ * @author Alexander Schade
+ *
+ */
+public class Launch {
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 *            The arguments. None supported yet.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Here we go...");
+	}
+
+}
